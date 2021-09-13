03357fc5a187419946918837867118c0406078dc
pcranaway <pcranaway@tuta.io>
2021-09-13 16:27:15 +0300

    refactoring

--- routes/index.go
+++ routes/index.go
@@ -6,10 +6,12 @@ import "github.com/flosch/pongo2/v4"
 var indexTemplate = pongo2.Must(pongo2.FromFile("templates/index.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    query := r.URL.Query()
+    params := r.URL.Query()
 
-    if query.Has("q") {
-        // we need to render search results
+    if params.Has("q") {
+        // we need to render search results=
+        // query := params.Get("q")
+
     } else {
         // we need to render the homepage
 
